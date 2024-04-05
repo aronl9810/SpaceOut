aba2a5e532c38d35fbe08a5770fc479791951d24
Aron Lin <aronl9810@hstat.org>
2024-04-05 13:33:31 -0400

    Finalized the UI

--- apcsa-project/Assets/Scripts/Player/PlayerAmmo.cs
+++ apcsa-project/Assets/Scripts/Player/PlayerAmmo.cs
@@ -11,21 +11,29 @@ public class PlayerAmmo : MonoBehaviour
     [SerializeField]
     private TextMeshProUGUI guntext;
     private int currentammo;
+    private float ammoUI;
+    private float initialammoUI;
     private string field;
+    public Image ammoCount;
     public Image reload;
     private float reloadtime;
+    private float initalreloadtime;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        initialammoUI = (float)grabGun.getAmmo();
+        initalreloadtime = grabGun.getReloadTime();
     }
 
     // Update is called once per frame
     void Update()
     {
         currentammo = grabGun.getAmmo();
+        ammoUI = (float)currentammo;
+        ammoCount.fillAmount = ammoUI / initialammoUI;
         reloadtime = grabGun.getReloadTime();
+        reload.fillAmount = (initalreloadtime - reloadtime)/ initalreloadtime;
         field = "" + currentammo + "";
         guntext.text = field;
     }
