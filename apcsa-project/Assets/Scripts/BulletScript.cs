7c1d181cb814e7aadb803e1f3ec50bd1781dde68
Aron Lin <aronl9810@hstat.org>
2024-05-06 21:46:12 -0400

    Enemies can now die :D

--- apcsa-project/Assets/Scripts/BulletScript.cs
+++ apcsa-project/Assets/Scripts/BulletScript.cs
@@ -12,9 +12,13 @@ public class BulletScript : MonoBehaviour
     } 
 
     void OnCollisionEnter(Collision collision){
+        Transform hitTransform = collision.transform;
         ContactPoint contact = collision.contacts[0];
         Vector3 position = contact.point;
         // Debug.Log(position);
+        if(hitTransform.CompareTag("Enemy")){
+            hitTransform.GetComponent<Enemy>().TakeDamage(20);
+        }
         Destroy(gameObject);
         bulletParticle.transform.position = position;
         bulletParticle.Play();
